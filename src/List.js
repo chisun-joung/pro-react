a907c70478efe39c65771b5910669956d84d176f
chisun-joung <csjoung@gmail.com>
2017-05-30 16:37:40 +0900

    chap3 PropTypes

--- src/List.js
+++ src/List.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Card from './Card';
 
 class List extends Component {
     render() {
         var cards = this.props.cards.map((card) => {
-            return <Card id={card.id}
+            return <Card key={card.id}
+                         id={card.id}
                          title={card.title}
                          description={card.description}
                          color={card.color}
@@ -19,5 +20,8 @@ class List extends Component {
         );
     }
 }
-
+List.propTypes = {
+    title: PropTypes.string.isRequired,
+    cards: PropTypes.arrayOf(PropTypes.object)
+}
 export default List;
