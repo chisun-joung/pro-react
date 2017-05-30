e591bcc57e628d313b69129373e173a9b952d911
chisun-joung <csjoung@gmail.com>
2017-05-30 11:12:12 +0900

    form ch2

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ let cardsList = [
     id: 1,
     title: "Read the Book",
     description: "I should read the whole book",
+    color: '#BD8D31',
     status: "in-progress",
     tasks: []
   },
   {
     id: 2,
     title: "Write some code",
-    description: "Code along with the samples in the book",
+    description: "Code along with the samples in the book. The Complete so at [github](https://github.com/pro-react)",
+    color: '#3A7E28',
     status: "todo",
     tasks: [
       {
